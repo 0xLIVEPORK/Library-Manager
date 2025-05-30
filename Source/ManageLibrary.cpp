030e444f68b1dccceafafa7273c4faec37efecaf
EVILPORK <0xlivepork@gmail.com>
2025-05-30 19:39:27 +0800

    Update ManageLibrary.cpp

--- Source/ManageLibrary.cpp
+++ Source/ManageLibrary.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+
+void set_bookdata();
+struct book_DATA
+{
+    std::string Title, 
+                Author, 
+                ISB, 
+                Genre;
+    
+    bool availability;
+    
+};
+
+void set_bookdata(book_DATA & book)
+    {
+        std::string dataentry;
+        std::string line;
+
+        while(true)
+            {
+                if(!(std::getline(std::cin, book.Author))
+                    {
+                        
+                    }
+            }
+    }
